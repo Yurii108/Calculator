ac49129d3197f3f7ea1ff154c23aec487dbea566
Yurii <paraska108@gmail.com>
2022-05-24 11:10:06 +0300

    push new files

---
+++ js/script.js
@@ -0,0 +1,20 @@
+'use-strict';
+
+// const one = document.querySelectorAll('button');
+
+// one.forEach((item) => {
+// item.addEventListener('click', () => {
+//         console.log('Ok');
+// });
+// }); 
+
+
+const one = document.querySelector('.one'),
+        two = document.querySelector('.two');
+
+one.addEventListener('click', () => {
+        console.log(1);
+});
+two.addEventListener('click', () => {
+        console.log(2);
+});
