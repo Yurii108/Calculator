c8743a048af586227ed783b145e5f2b547765aa4
Yurii <paraska108@gmail.com>
2022-05-25 12:31:11 +0300

    add funcrion

--- js/script.js
+++ js/script.js
@@ -1,20 +1,87 @@
 'use-strict';
 
-// const one = document.querySelectorAll('button');
+// const form = document.querySelector('form');
+let display = document.querySelector('.display'),
+        allCancel = document.querySelector('.ac'),
+        one = document.querySelector('.one'),
+        two = document.querySelector('.two'),
+        plus = document.querySelector('.plus'),
+        sumAnswer = document.querySelector('.sum_answer');
 
-// one.forEach((item) => {
-// item.addEventListener('click', () => {
-//         console.log('Ok');
-// });
-// }); 
+// let input = document.querySelector('input');
+const allButton = document.querySelectorAll('button');
 
-
-const one = document.querySelector('.one'),
-        two = document.querySelector('.two');
-
-one.addEventListener('click', () => {
-        console.log(1);
+let sumDisplay = ''; /////////////////committttttttttttttttttttttttttttttttttttt
+let sum = 0;
+allButton.forEach((item) => {
+        item.addEventListener('click', (event) => {
+                event.preventDefault();
+                // if (item === allCancel) {
+                //         num += 1;
+                //         console.log(num);
+                // }
+                if (item === allCancel) {
+                        display.innerHTML = 0;
+                        sum = 0;
+                        sumDisplay = '';
+                }
+                if (item === sumAnswer) {
+                        display.innerHTML = sum;
+                }
+                if (item === one) {
+                        sumDisplay += 1;
+                        display.innerHTML = sumDisplay;
+                        sum += 1;
+                        // console.log(sum);
+                }
+                if (item === two) {
+                        sumDisplay += 2;
+                        display.innerHTML = sumDisplay;
+                        sum += 2;
+                        // console.log(sum);
+                }
+                if (item === plus) {
+                        display.innerHTML = sum;
+                        sumDisplay += '+';
+                }
+                console.log(sum);
+        });
 });
+
+function countSum(a, b) {
+        return a + b;
+}
+
+// input.addEventListener('click', (event) => {
+//         event.preventDefault();
+//         let v = input.value;
+//         console.log(v);
+
+// });
+
+let numbers = 0;
+
+// one.addEventListener('click', (event) => {
+//         event.preventDefault();
+//         numbers = input.value;
+//         console.log(numbers);
+// });
 two.addEventListener('click', () => {
         console.log(2);
 });
+
+
+
+
+// function scrollingText(word) {
+//         let words = [];
+
+//         for (let i = 0; i < word.length; i++) {
+//                 words += word[i];
+
+//                 // words += word[i];
+//                 // words.push(w);
+//         }
+//         return  (words.toUpperCase());
+// }
+// console.log(scrollingText('robot'));
