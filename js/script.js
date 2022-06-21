10371de9606fc47c0a5c0a24cd4cfee8967498bb
Yurii <paraska108@gmail.com>
2022-06-21 16:40:33 +0300

    add whatch

--- js/script.js
+++ js/script.js
@@ -2,6 +2,30 @@
 
 window.addEventListener('DOMContentLoaded', function () {
 
+    const timeInterval = setInterval(upDateTime, 1000);
+
+    function upDateTime() {
+        const watchOnPage = document.querySelector('.watch');
+        let t = new Date();
+        let hours, mins, seconds;
+        
+        hours = Math.floor(t / (1000 * 60 * 60) % 24) + 3;
+        mins = Math.floor(t / (1000 * 60) % 60);
+        seconds = Math.floor((t / 1000) % 60);
+        
+        watchOnPage.textContent = `${hours}:${plusZero(mins)}:${plusZero(seconds)}`;
+    
+        function plusZero(n) {
+            if (n < 10) {
+                return `0${n}`; 
+            } else {
+                return n;
+            }
+        }
+    }
+
+
+    //Tabs
     const tabs = document.querySelectorAll('.tabheader_item'),
         tabsContent = document.querySelectorAll('.content'),
         tabsParent = document.querySelector('.tabheader_items');
@@ -25,11 +49,6 @@ window.addEventListener('DOMContentLoaded', function () {
         hideTabsContent();
         showTabsContent();
 
-        if (tabsParent) {
-            console.log('ok');
-        } else {
-            console.log('erorr');
-        }
 
         tabsParent.addEventListener('click', (e) => {
             const target = e.target;
